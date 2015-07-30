7b0a795046f56bdbd58aaf523e46cadffaec35fa
Ryan Delaney <rrdelaney@outlook.com>
2015-07-30 16:17:22 -0400

    Added task

--- index.js
+++
@@ -1,4 +0,0 @@
-module.exports = function() {
-    console.log("Placeholder for jadelint :)");
-    return undefined;
-}
---
+++ tasks/jadelint.js
@@ -0,0 +1,32 @@
+var _ = require('lodash');
+var jadelint = require('jadelint');
+
+module.exports = function(grunt) {
+    grunt.registerMultiTask('jadelint', 'Validate Jade code', function() {
+        var options = this.options({
+            configFile: '.jadelintrc',
+            config: {}
+        });
+
+        if (options.configFile) {
+            var config = grunt.file.readJSON(options.configFile);
+            options.config = _.extend({}, config, options.config);
+        }
+
+        var success = true;
+        this.filesSrc.forEach(function(file) {
+            var linter = new jadelint(file, grunt.file.read(file));
+            _.forEach(linter.lint(), function(error) {
+                if (error.level === 'error') {
+                    success = false;
+                }
+
+                if (error.level !== 'ignore') {
+                    grunt.log.error(error.filename + ':' + error.line + '\n    ' + error.name);
+                }
+            });
+        });
+
+        return success;
+    });
+};
